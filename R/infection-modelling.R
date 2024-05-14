3e6f0dcc175ef4aa46441289e4703a10d9253dae
David.Ellis <david.ellis@birmingham.gov.uk>
2024-05-14 17:08:23 +0100

    Added single birth/death rate and immunity loss rate

--- R/infection-modelling.R
+++ R/infection-modelling.R
@@ -10,7 +10,7 @@ SEIR_model <- function(
   ) {
   
   # Check and fill missing elements with zero
-  for (elem in c("lamda", "gamma", "sigma", "vac")) {
+  for (elem in c("beta", "gamma", "sigma", "vac", "omega", "mu", "alpha")) {
     if (!(elem %in% names(params))) {
       params[[elem]] <- 0
     }
@@ -27,17 +27,49 @@ SEIR_model <- function(
   e = rep(0, timesteps)
   i = rep(0, timesteps)
   r = rep(0, timesteps)
+  d = rep(0, timesteps)
   
-  # initial infective people
+  infected_sum = rep(0, timesteps)
+  
+  # initial infected people
   i[1] = inits$i
   s[1] = inits$s
   e[1] = inits$e
+  r[1] = inits$r
   
   for (t in 1:(T-1)){
-    s[t + 1] = s[t] - lamda * s[t] * i[t] - vac * s[t]
-    e[t + 1] = e[t] + lamda * s[t] * i[t] - sigma * e[t]
-    i[t + 1] = i[t] + sigma * e[t] - gamma * i[t]
-    r[t + 1] = r[t] + gamma * i[t] + vac * s[t]
+    
+    s[t + 1] = s[t] +
+     params$mu - # births
+     params$beta * s[t] * i[t] + # infection
+     params$omega * r[t] - # lost immunity
+     params$vac * s[t] - # vaccination
+     params$mu * s[t] # death
+    
+    e[t + 1] = e[t] +
+     params$beta * s[t] * i[t] - # infection
+     params$sigma * e[t] - # latency
+     params$mu * e[t] # death
+    
+    i[t + 1] = i[t] +
+     params$sigma * e[t] - # latency
+     params$gamma * i[t] - # recovery
+     (params$mu + params$alpha) * i[t] # death
+    
+    r[t + 1] = r[t] +
+     params$gamma * i[t] + # recovery
+     params$vac * s[t] - # vaccination
+     params$omega * r[t] - # lost immunity
+     params$mu * r[t] # death
+    
+    d[t+1] = d[t] +
+     params$mu * s[t] +# death of susceptible
+     params$mu * e[t] +# death of exposed
+     (params$mu + params$alpha) * i[t] +# death of infected
+     params$mu * r[t] # death of recovered
+    
+    infected_sum[t+1] = infected_sum[t] +
+       params$beta * s[t] * i[t]
   }
   
   df <- data.frame(
@@ -45,7 +77,9 @@ SEIR_model <- function(
     "susceptible" = s,
     "exposed" = e,
     "infectious" = i,
-    "recovered" = r
+    "recovered" = r,
+    "died" = d,
+    "infected sum" = infected_sum
   ) 
   
   return(df)
@@ -53,24 +87,30 @@ SEIR_model <- function(
 
 params <- list(
   # contact rate
-  lamda = 0.5,
+  beta = 0.5,
   # recover rate
   gamma = 0.0821,
   # exposed period
   sigma = 1 / 4,
   # vaccination rate
-  vac = 0.006
+  vac = 0.006,
+  # immunity loss rate
+  omega = 0.0001,
+  # daily birth/death rate
+  mu = 0.001,
+  # infected loss of life
+  alpha = 0.00000001
 )
 
 inits <- list(
   # susceptiable ratio
-  s = 0.99,
+  s = 0.3,
   # exposed ratio
   e = 0,
   # infective ratio
   i = 0.01,
   # recovered ratio
-  r = 0
+  r = 0.7
 )
 
 # simuation Time / Day
@@ -79,10 +119,10 @@ tsteps = 170
 df <- SEIR_model(params, inits, tsteps) %>%
   reshape2::melt(id.vars = "time", 
        variable.name = 'Group',
-       value.name = 'Number')
+       value.name = 'Fraction')
 
-plt <- ggplot(df, aes(x = time, y = Number, col = Group)) +
-  geom_line(linewidth = 1.2) +
+plt <- ggplot(df, aes(x = time, y = Fraction, col = Group)) +
+  geom_line(linewidth = 1.1) +
   theme_bw() +
   ggtitle("Vaccination rate = 0.002") + theme(
     plot.title = element_text(hjust = 0.5)
